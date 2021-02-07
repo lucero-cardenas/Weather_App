651e6d7f426786c5debb8d52ecaffa39c2516d20
lucero-cardenas <76122129+lucero-cardenas@users.noreply.github.com>
2021-02-07 02:35:43 +0100

    layout and JS improved
    Layout improved
    
    JS improved for easier unit conversion:
    Each time it changes, the data is read again from the API rather than convert the numbers locally.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ function showCityTemp(cityTemp){
   let country = cityTemp.data.sys.country;
   let city = cityTemp.data.name;
 
-  let temp = Math.round(cityTemp.data.main.temp);
-  let wind = cityTemp.data.wind.speed;
-  let humidity = cityTemp.data.main.humidity;
+  globalCity = city; //for later use of unit conversion
+
   let cityTitle = document.querySelector("#shown-city");
   cityTitle.innerHTML = `${city}, ${country}`;
+
+  let temp = Math.round(cityTemp.data.main.temp);
+  console.log(temp);
   let newCityTemp = document.querySelector("#curr-temp");
   newCityTemp.innerHTML = `${temp}`;
+  let wind = cityTemp.data.wind.speed;
   let newWind = document.querySelector("#wind");
   newWind.innerHTML = `Wind speed: ${wind}`;
+  let humidity = cityTemp.data.main.humidity;
   let newHumidity = document.querySelector("#humidity");
   newHumidity.innerHTML = `Humidity: ${humidity}`;
+  
+  let description = cityTemp.data.weather[0].description;
+  let newDescription = document.querySelector("#description");
+  newDescription.innerHTML = `Description: ${description}`;
+
+  let feelsLike = Math.round(cityTemp.data.main.feels_like);
+  let newFeelsLike = document.querySelector("#feels-like");
+  newFeelsLike.innerHTML = `Feels like: ${feelsLike}`;
 }
 
 function changeCity (event) {
@@ -20,21 +32,30 @@ function changeCity (event) {
   let newCity = document.querySelector("#city-input");
   newCity = newCity.value; // to get the string from the #city-input form
   newCity = newCity.trim();
-  //newCity = newCity.toLowercase();
-  let apiKey = "097f9479820416d5a2f6eb074cd4d616";
-  let units = "metric";
+  let units = null;
+  if (celciusUnit == true) {
+    units = "metric";
+  } else {
+    units = "imperial";
+  }
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=${units}&appid=${apiKey}`
   axios.get(apiUrl).then(showCityTemp);
+  console.log(apiUrl);
 }
 
 function myPosition(position){
   console.log(position.coords);
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let apiKey = "097f9479820416d5a2f6eb074cd4d616";
-  let units = "metric";
+  let units = null;
+  if (celciusUnit == true) {
+    units = "metric";
+  } else {
+    units = "imperial";
+  }
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`
   axios.get(apiUrl).then(showCityTemp);
+  console.log(apiUrl);
 }
 function getPosition(){
   navigator.geolocation.getCurrentPosition(myPosition);
@@ -42,30 +63,57 @@ function getPosition(){
 
 function changeUnitToF(event){
   event.preventDefault();
+  celciusUnit = false; // for further searches
+
   let cLink = document.querySelector("#unit-c");
   cLink.classList.remove("strong");
   let flink = document.querySelector("#unit-f");
   flink.classList.add("strong");
-  let newUnitTemp = document.querySelector("#curr-temp");
-  newUnitTemp = Number(newUnitTemp);
-  newUnitTemp.innerHTML = (newUnitTemp - 32) * 5/9;
+
+  let fLink = document.querySelector("#unit-f");
+  fLink.classList.remove("soft");
+  let clink = document.querySelector("#unit-c");
+  clink.classList.add("soft");
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${globalCity}&units=imperial&appid=${apiKey}`
+  axios.get(apiUrl).then(showCityTemp);
 }
 
 function changeUnitToC(event){
   event.preventDefault();
+  celciusUnit = true; //for further searches
+
   let fLink = document.querySelector("#unit-f");
   fLink.classList.remove("strong");
   let clink = document.querySelector("#unit-c");
   clink.classList.add("strong");
-  let newUnitTemp = document.querySelector("#curr-temp");
-  newUnitTemp = Number(newUnitTemp);
-  newUnitTemp.innerHTML = newUnitTemp * 9/5 + 32;
+
+  let cLink = document.querySelector("#unit-c");
+  cLink.classList.remove("soft");
+  let flink = document.querySelector("#unit-f");
+  flink.classList.add("soft");
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${globalCity}&units=metric&appid=${apiKey}`
+  axios.get(apiUrl).then(showCityTemp);
 }
+
+//Global variables
+let apiKey = "097f9479820416d5a2f6eb074cd4d616";
+let globalCity = null;
+let celciusUnit = true;
+
+getPosition(); //Start from your current position
 
 //Current Date
 let currentDateTime = new Date();
 let hours = currentDateTime.getHours();
+if (hours.toString().length < 2) {
+  hours = `0${hours}`;
+}
 let minutes = currentDateTime.getMinutes();
+if (minutes.toString().length < 2) {
+  minutes = `0${minutes}`;
+}
 let days = ["Sun", "Mon", "Tue", "Thu", "Fri", "Sat"];
 let day = days[currentDateTime.getDay()];
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -74,7 +122,7 @@ let date = currentDateTime.getDate();
 let year = currentDateTime.getFullYear();
 
 let changeDate = document.querySelector("#current-date-time");
-changeDate.innerHTML = `Today - ${day}, ${month} ${date}, ${year}, ${hours}:${minutes}`;
+changeDate.innerHTML = `${day}, ${month} ${date}, ${year}, ${hours}:${minutes}`;
 
 //City Lookup
 let submitCity = document.querySelector("#city-search-form");
@@ -84,7 +132,7 @@ submitCity.addEventListener("submit", changeCity);
 let currentLoc = document.querySelector("#current_location");
 currentLoc.addEventListener("click", getPosition);
 
-//3rd Step
+//Unit switch
 let unitToF = document.querySelector ("#unit-f");
 unitToF.addEventListener("click", changeUnitToF);
 
